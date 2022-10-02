bcd30bc4b7e42b27b4d4ed8591e1d326a46018c7
Bogdan <56205387+edge-py@users.noreply.github.com>
2022-10-02 18:09:03 +0300

    created home view of products stored in database

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Product
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    product_list = Product.objects.get_queryset()
+    context = {
+        'product_list': product_list,
+    }
+    return render(request, "home.html", context)
