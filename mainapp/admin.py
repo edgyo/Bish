03c4bce99680d20cf605f1a6b751b751850352ac
Bogdan <56205387+edge-py@users.noreply.github.com>
2022-10-02 17:46:14 +0300

    created test home page

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
-
 from .models import Product
-admin.site.register(Product)
+
+
+# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    fields = ("name", "type", "price") # Fields to use for add/edit/show page
+    list_display = ("name", "type", "price", "updated_at") # fields to display in search page
+
+# Register app
+admin.site.register(Product, ProductAdmin)
