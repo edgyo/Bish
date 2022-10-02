14db5586e27f7a81e78c2024da353eb14de03c00
Bogdan <56205387+edge-py@users.noreply.github.com>
2022-10-02 17:22:43 +0300

    Created Product model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,15 @@
+from email.policy import default
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length = 120)
+    price = models.FloatField(default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    TYPE = (
+        ('Food', 'Food'),
+        ('Drinks', 'Drinks'),
+    )
