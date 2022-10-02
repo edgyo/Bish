d5801abdeeddab09f51944b895cfd187116eac8c
Bogdan <56205387+edge-py@users.noreply.github.com>
2022-10-02 20:40:35 +0300

    updated product model, created manufacturer model

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,13 +1,17 @@
-from email.policy import default
 from django.db import models
 
 # Create your models here.
-
+class Manufacturer(models.Model):
+    name = models.CharField(max_length = 60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
 class Product(models.Model):
     name = models.CharField(max_length = 120)
     price = models.FloatField(default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
 
     TYPE_CHOICES = (
         ('Food', 'Food'),
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+from sqlite3 import OperationalError
 from django.shortcuts import render
 from django.http import Http404
 
@@ -9,8 +10,11 @@ def home(request):
     return render(request, "home.html")
 
 def shop(request):
-    product_list = Product.objects.get_queryset()
-    context = {
+    try:
+        product_list = Product.objects.get_queryset()
+        context = {
         'product_list': product_list,
     }
+    except OperationalError:
+        return Http404("No items currently available")
     return render(request, "shop.html", context)
